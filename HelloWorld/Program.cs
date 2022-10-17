7cef3b3d903563a9e5ed2c913792aa7784b3b04b
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-10-17 19:54:42 +0530

    HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class HelloWorld
+{
+    public static void display()
+
+    {
+        Console.WriteLine("Hello World!");
+    }
+        static void Main(string[] args)
+        {
+        HelloWorld.display();
+        }
+    
+}
